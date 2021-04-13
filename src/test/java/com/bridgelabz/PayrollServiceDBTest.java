b1c3b1bccb0a2fed69c30016f19b3efed1260821
joshivinit2018 <joshivinit2018@gmailcom>
2021-04-13 15:31:56 +0530

    [vinit]:retrive code to retrieve data from database

--- src/test/java/com/bridgelabz/PayrollServiceDBTest.java
+++ src/test/java/com/bridgelabz/PayrollServiceDBTest.java
@@ -1,6 +1,15 @@
 package com.bridgelabz;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 public class PayrollServiceDBTest {
-
-
+    @Test
+    void givenEmployeePayrollDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+        Assertions.assertEquals(3, employeePayrollData.size());
+    }
 }
